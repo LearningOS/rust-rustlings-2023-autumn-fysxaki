f4c18816483a9bd0b6be7c4b42d71e8fbce23ca2
fysxq <baetsu314@gmail.com>
2023-10-09 14:33:25 +0000

    0.0.03

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,9 +16,9 @@
 // I AM NOT DONE
 
 // Put your function here!
-//fn calculate_price_of_apples {
+fn calculate_price_of_apples: i8 {
     
-//}
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {

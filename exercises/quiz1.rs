77d95d5f345578bba6cddd36984c73c23cd1ed61
fysxq <baetsu314@gmail.com>
2023-10-09 15:37:58 +0000

    0.0.04-23-10-9

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-fn calculate_price_of_apples: i8 {
-    
+fn calculate_price_of_apples(num:i8) -> i8 {
+    if num > 40 {num}
+    else {num * 2}
 }
 // Don't modify this function!
 #[test]

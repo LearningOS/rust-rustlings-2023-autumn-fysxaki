5d690b8e4e3c9ac2dc9921e542af309aa76b8deb
fysxq <baetsu314@gmail.com>
2023-10-19 14:02:33 +0000

    0.0.14-finsihed(Painful but rewarding version)

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,25 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(Self::Err::Empty);
+        }
+
+        let v:Vec<_> = s.split(",").collect();
+        if v.len() != 2 {
+            return Err(Self::Err::BadLen);
+        }
+        if v[0].len() == 0 {
+            return Err(Self::Err::NoName);
+        }
+
+        match v[1].parse() {
+            Ok(age) =>Ok(Person {
+                name: String::from(v[0]),
+                age: age
+            }),
+            Err(err) => Err(Self::Err::ParseInt(err))
+        }
     }
 }
 
--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,7 +8,7 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -24,9 +24,16 @@ fn main() {
         }));
     }
 
+    
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        // handle.join().unwrap();
+        // results.push(1);
+        match handle.join() {
+            Ok(result) => results.push(result),
+            Err(e) => println!("Thread panicked: {:?}", e),
+        }
     }
 
     if results.len() != 10 {
